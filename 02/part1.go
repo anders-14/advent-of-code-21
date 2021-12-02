0a8c216c47cb3e1104b31192a2f6d99d35b195eb
Anders Fimreite <anders.fimreite@gmail.com>
2021-12-02 16:47:40 +0100

    Day 2 done - 23 to go

---
+++ 02/part1.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInput() []string {
+	var ret []string
+
+	file, _ := os.Open("input")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		ret = append(ret, scanner.Text())
+	}
+
+	return ret
+}
+
+func main() {
+	input := readInput()
+
+	horizontal_pos := 0
+	depth := 0
+
+	for _, line := range input {
+		cmd := strings.Split(line, " ")
+		dist, _ := strconv.Atoi(cmd[1])
+
+		switch cmd[0] {
+		case "forward":
+			horizontal_pos += dist
+			break
+		case "up":
+			depth -= dist
+			break
+		case "down":
+			depth += dist
+			break
+		}
+	}
+
+	fmt.Printf("Horizontal position: %d\n", horizontal_pos)
+	fmt.Printf("Depth:               %d\n", depth)
+  fmt.Printf("Product:             %d\n", horizontal_pos * depth)
+}
